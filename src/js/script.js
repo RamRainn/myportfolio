96bdc63a14fb079a508273ac9b726b961e89cc21
Ramil <ramrain.411@gmail.com>
2021-03-05 01:28:25 +0500

    pre final

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,29 @@
+
+// Меню гамбургер
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () =>{
     menu.classList.add('active');
+    document.body.style.overflow = 'hidden';
 });
 
 closeElem.addEventListener('click', () =>{
     menu.classList.remove('active');
+    document.body.style.overflow = '';
 });
 
+menu.addEventListener('click', (e) => {
+    if (e.target === menu) {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    }
+});
+
+
+// Скилы 
 const counters = document.querySelectorAll('.skills__ratings-counter'),
       lines = document.querySelectorAll('.skills__ratings-line span');
 
@@ -17,3 +31,4 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
